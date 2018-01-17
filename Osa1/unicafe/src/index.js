c0e4549e4bb30c0df7d90f4827e79fd03269a951
Jonas Harjunpää <jonas.harjunpaa@helsinki.fi>
2018-01-17 18:32:12 +0200

    tehtävä 1.7 tehty

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -7,27 +7,32 @@ class App extends React.Component {
       this.state = {
         hyva: 0,
         neutraali:0,
-        huono:0
+        huono:0,
+        keskiarvo:0,
+        kaikkiaan:0
       }
     }
-        
-    render(){    
+    
+    render(){
+         
       return (
         <div>
           <h1>Anna palautetta</h1>
-          <button onClick={() => this.setState({hyva: this.state.hyva +1})}>
+          <button onClick={() => this.setState({hyva: this.state.hyva +1, keskiarvo: this.state.keskiarvo +1, kaikkiaan: this.state.kaikkiaan +1})}>
             Hyvä
           </button>
-          <button onClick={() => this.setState({neutraali: this.state.neutraali +1})}>
+          <button onClick={() => this.setState({neutraali: this.state.neutraali +1, kaikkiaan: this.state.kaikkiaan +1})}>
             Neutraali
           </button>
-          <button onClick={() => this.setState({huono: this.state.huono +1})}>
+          <button onClick={() => this.setState({huono: this.state.huono +1, keskiarvo: this.state.keskiarvo -1, kaikkiaan: this.state.kaikkiaan +1})}>
             Huono
           </button>
           <h2>Statistiikkaa</h2> 
           <p>Hyvä:{this.state.hyva}</p>
           <p>Neutraali:{this.state.neutraali}</p>
           <p>Huono:{this.state.huono}</p>
+          <p>Keskiarvo:{this.state.keskiarvo/this.state.kaikkiaan}</p>
+          <p>Positiivisia:{100*this.state.hyva/this.state.kaikkiaan}%</p>
         </div>
         )
   }
