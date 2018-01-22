6dab7931272b494569c8d0e3b352d12e7ef0b2f0
Jonas Harjunpää <jonas.harjunpaa@helsinki.fi>
2018-01-22 20:13:04 +0200

    tehtävä 2.12 tehty

---
+++ Osa2/maat/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      string: "",
+      countries:[]
+    }
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        this.setState({ countries: response.data })
+      })
+  }
+
+  handleStringChange = (event) => {
+    this.setState({ string: event.target.value })
+  }
+
+  render() {
+    const countriesToShow = this.state.countries.filter(country => country.name.toLocaleLowerCase().includes(this.state.string.toLocaleLowerCase()))
+    return (
+      <div>
+        Find countries: <input value = {this.state.string} onChange={this.handleStringChange} />
+        <Results list = {countriesToShow} />
+      </div> 
+    );
+  }
+}
+const Results = (props) => {
+  if(props.list.length === 0){
+    return(
+      <div>
+      </div>
+    )
+  } else if(props.list.length > 9){
+    return(
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  } else if (props.list.length > 1){
+    const rivit = () => props.list.map(country => <li key = {country.name}>{country.name}</li>)
+    return(
+      <div>
+        <ul>
+          {rivit()}
+        </ul>
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <h2>{props.list[0].name} {props.list[0].nativeName}</h2>
+        <p>Capital: {props.list[0].capital}</p>
+        <p>Populatuion: {props.list[0].population}</p>
+        <img src={props.list[0].flag} alt='The countries flag' height="50%" width="50%" border='2'/>
+      </div>
+    )
+  }
+  
+}
+
+export default App;
