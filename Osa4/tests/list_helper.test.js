eb7ee6900f888dcddbca325349b611028ec012c8
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-02-09 18:19:56 +0200

    tehtävä 4.4 tehty

---
+++ Osa4/tests/list_helper.test.js
@@ -0,0 +1,51 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy is called', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const listWithBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+  test('when list is empty the likes are 0', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes([listWithBlogs[0]])
+    expect(result).toBe(5)
+  })
+  
+  test('when list has many blogs the sum equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithBlogs)
+    expect(result).toBe(7)
+  })
+})
