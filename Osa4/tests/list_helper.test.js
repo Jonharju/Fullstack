52c84f61f6cc106e05eab8c6038d156ebfd892b9
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-02-09 19:03:44 +0200

    tehtävä 4.5 tehty

--- Osa4/tests/list_helper.test.js
+++ Osa4/tests/list_helper.test.js
@@ -49,3 +49,57 @@ describe('total likes', () => {
     expect(result).toBe(7)
   })
 })
+
+
+describe('favorite blog', () => {
+  const listWithBlogs = [
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+  test('when list is empty return empty array', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({})
+  })
+
+  test('when list has only one blog return its values', () => {
+    const result = listHelper.favoriteBlog([listWithBlogs[2]])
+    const expected = {
+      title: "Type wars",
+      author: "Robert C. Martin",
+      likes: 2
+    }
+    expect(result).toEqual(expected)
+  })
+  
+  test('when list has many blogs return the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithBlogs)
+    const expected = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    }
+    expect(result).toEqual(expected)
+  })
+})
--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -9,7 +9,22 @@ const totalLikes = (blogs) => {
     return blogs.length === 0 ? 0 : blogs.reduce(reducer,0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (previous, current) => {
+        return (previous.likes > current.likes) ? previous : current
+    }
+    if(blogs.length === 0) return {}
+    
+    const fav = blogs.reduce(reducer)
+    return {
+        title: fav.title,
+        author: fav.author,
+        likes: fav.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
