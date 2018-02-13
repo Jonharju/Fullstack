e9800847d078d5f91f0a3de9cb3dfc0fd36ee795
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-02-13 10:47:07 +0200

    tehtävä 4.18 tehty

--- Osa4/index.js
+++ Osa4/index.js
@@ -7,12 +7,15 @@ const mongoose = require('mongoose')
 const blogsRouter = require("./controllers/blogs")
 const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
