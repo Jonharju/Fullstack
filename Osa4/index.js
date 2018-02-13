ee9bbb22f3d8f3ece8f5e1ca7233179117a74100
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-02-13 20:29:22 +0200

    tehtävä 4.20 tehty

--- Osa4/index.js
+++ Osa4/index.js
@@ -9,10 +9,18 @@ const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
 
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
