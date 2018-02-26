af63d89c4a4af3db62f120c4ab23a4b41a86c8f2
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-02-26 20:45:22 +0200

    tehtävä 5.1 tehty

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-
+import loginService from './services/login'
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      user: null,
+      username: '',
+      password: '',
+      error: null
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    if (event.target.name === 'password') {
+      this.setState({ password: event.target.value })
+    } else if (event.target.name === 'username') {
+      this.setState({ username: event.target.value })
+    }
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+  
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
     }
   }
 
@@ -17,14 +48,44 @@ class App extends React.Component {
   } 
 
   render() {
+    if (this.state.user === null) {
+      return (
+        <div>
+          <h2>Kirjaudu sovellukseen</h2>
+          <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">kirjaudu</button>
+        </form>
+        </div>
+      )
+    }
+  
     return (
       <div>
         <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
+        <p>{this.state.user.name} logged in</p>
+        {this.state.blogs.map(blog =>
+          <Blog key={blog._id} blog={blog} />
         )}
       </div>
-    );
+    )
   }
 }
 
