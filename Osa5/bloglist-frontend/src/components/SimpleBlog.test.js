265e2b4e8a92c8205e3e71c7272e3816c058a021
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-02-28 18:34:38 +0200

    tehtävä 5.13 tehty

--- Osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ Osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -19,4 +19,26 @@ describe.only('<SimpleBlog />', () => {
       const likeDiv = blogComponent.find('.likes')
       expect(likeDiv.text()).toContain(blog.likes)
     })
+
+    it('liking the blog twice calls event handler twice', () => {
+        const blog = {
+            title: 'Title',
+            author: 'Author',
+            likes: 1
+        }
+      
+        const mockHandler = jest.fn()
+      
+        const blogComponent = shallow(
+            <SimpleBlog 
+            blog={blog}
+            onClick={mockHandler}
+            />
+        )
+      
+        const button = blogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+        expect(mockHandler.mock.calls.length).toBe(2)
+      })
   })
