c859dba7ed661de9e0e91fd1580d192c0b67af4b
Jonharju <jonas.harjunpaa@helsinki.fi>
2018-03-08 12:17:45 +0200

    tehtävä 6.22 tehty

--- Osa6/routed-anecdotes/src/App.js
+++ Osa6/routed-anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Grid, Col, Thumbnail } from 'react-bootstrap'
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
+import image from './Babbage40.png'
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
@@ -31,14 +32,21 @@ const Anecdote = ({anecdote}) => {
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
+    <Grid className="show-grid">
+      <Col md={6}>
+        <p>According to Wikipedia:</p>
+        
+        <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+          Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+          such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+          An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </Col>
+      <Col md={3}>
+        <Thumbnail src={image} responsive />
+      </Col>
+    </Grid>
   </div>
 )
 
